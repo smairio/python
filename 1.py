c3a38add02f134cf0e50c80b34a670ef9ccc479c
khalil smairi <smairio98@gmail.com>
2021-02-01 17:00:36 -0800

    1

---
+++ 1.py
@@ -0,0 +1,7 @@
+a=float(input("donner a : "))
+b=float(input("donner b : "))
+c=float(input("donner c : "))
+moy = (a+b+c)/3
+print("le moyenne est = {}".format(moy))
+print("le moyenne est = {:.3f}".format(moy))
+print("le moyenne est = ",moy)
